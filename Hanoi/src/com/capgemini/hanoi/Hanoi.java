824befe70540fd93945f5f2183e9bc3bc00ec453
Bogna Wróbel <bogna.wrobel@capgemini.com>
2017-02-11 16:46:23 +0100

    working hanoi without tests, fibonacci to be edited

--- Hanoi/src/com/capgemini/hanoi/Hanoi.java
+++ Hanoi/src/com/capgemini/hanoi/Hanoi.java
@@ -1,121 +1,150 @@
 package com.capgemini.hanoi;
+
 import java.util.Scanner;
 import java.util.Set;
 import java.util.ArrayList;
 import java.util.HashSet;
 
+/**
+ * makes playing Hanoi towers game possible. Creates the board and collects
+ * commands of a user through a console.
+ * 
+ * @author BOWROBEL
+ *
+ */
 public class Hanoi {
 
-	public static Stack originTower;
-	public static Stack intermediateTower;
-	public static Stack targetTower;
-	public static ArrayList<Integer> listOfRings=new ArrayList<>();
-	private static Set<Integer> indexesOfTowers=new HashSet<Integer>();
-	
+	private static Stack originTower;
+	private static Stack intermediateTower;
+	private static Stack targetTower;
+	private static ArrayList<Integer> listOfRings = new ArrayList<>();
+	private static Set<Integer> indexesOfTowers = new HashSet<Integer>();
+	private static ArrayList<Integer[]> listOfMoves = new ArrayList<>();
 
-	// creating list of rings - first is the biggest one, then in descending order
-	public static ArrayList<Integer> createRings(int numberOfRings) {
-		
-		for (int i = numberOfRings; i >=1; i--) {
+	/**
+	 * creates the board and runs the game
+	 */
+	public static void setBoard() {
+		indexesOfTowers.add(1);
+		indexesOfTowers.add(2);
+		indexesOfTowers.add(3);
+
+		System.out.println("Welcome to Hanoi Towers game, you have three empty towers numbered 1, 2 and 3.");
+		System.out.println("How many rings do you want to put on the tower number 1?");
+		Scanner sc = new Scanner(System.in);
+		int numberOfRings = sc.nextInt();
+		listOfRings = createRings(numberOfRings);
+		createTowers(listOfRings);
+		System.out.println("Tower 1 looks like this:");
+		originTower.displayVertical();
+		Hanoi.getCommandsForMovingRings();
+		System.out.println("Congratulations! You finished the game.");
+		printListOfMoves();
+		sc.close();
+	}
+
+	/**
+	 * creates list of rings - first is the biggest one, then in descending
+	 * order
+	 * 
+	 * @param numberOfRings
+	 * @return ArrayList of integer values meaning rings diameter
+	 */
+	private static ArrayList<Integer> createRings(int numberOfRings) {
+
+		for (int i = numberOfRings; i >= 1; i--) {
 			listOfRings.add(i);
 		}
 		return listOfRings;
 	}
-	public static void showListOfRings(ArrayList<Integer> listOfRings){
-		for (Integer k : listOfRings) {
-			System.out.println(k);
-		}
-	}
-	
-	//adding rings to first tower and creating other towers
-	public static void createTowers(ArrayList<Integer> listOfRings){
-		intermediateTower=new Stack(listOfRings.size());
-		targetTower=new Stack(listOfRings.size());
-		originTower=new Stack(listOfRings.size());
+
+	/**
+	 * puts rings on the first tower from the biggest to the smallest and
+	 * creates the other two
+	 * 
+	 * @param listOfRings
+	 */
+	private static void createTowers(ArrayList<Integer> listOfRings) {
+		intermediateTower = new Stack(listOfRings.size());
+		targetTower = new Stack(listOfRings.size());
+		originTower = new Stack(listOfRings.size());
 		for (int i = 0; i < listOfRings.size(); i++) {
 			originTower.push(listOfRings.get(i).intValue());
 		}
 	}
-	
-	
-	public static void moveRing(Stack origin, Stack target){
-		if(origin.isEmpty()){
-			System.out.println("The tower is empty! "
-					+ "\n Choose another move.");
+
+	/**
+	 * collects commands for moving rings in form of tower numbers
+	 */
+	private static void getCommandsForMovingRings() {
+		Scanner sc = new Scanner(System.in);
+		while (targetTower.getSize() < targetTower.getMaximumStackSize() - 1) {
+			int originTowerNumber, targetTowerNumber;
+			System.out.println("Choose a tower to take a ring from: ");
+			originTowerNumber = sc.nextInt();
+			System.out.println("Choose a tower to put a ring on: ");
+			targetTowerNumber = sc.nextInt();
+			Stack[] allTowers = { originTower, intermediateTower, targetTower };
+			moveRingIfTowerNumberIsValid(originTowerNumber, targetTowerNumber, allTowers);
+
 		}
-		else{
-			if(target.isEmpty()||origin.peek()<target.peek()){
-				//System.out.println(origin.peek()); //podglad wierzcholka pierwotnej
-				int temp=origin.pop();
-				//System.out.println(origin.peek()); //podglad wierzcholka pierwotnej po zdjeciu elementu
-				target.push(temp);
-				//System.out.println(target.peek());
+		sc.close();
+	}
+
+	/**
+	 * validates tower numbers entered by player, if in range of 1-3, it
+	 * attempts to move the ring, if not it prints a message to the user
+	 * 
+	 * @param originTowerNumber
+	 * @param targetTowerNumber
+	 * @param allTowers
+	 */
+	private static void moveRingIfTowerNumberIsValid(int originTowerNumber, int targetTowerNumber, Stack[] allTowers) {
+		if (indexesOfTowers.contains(originTowerNumber) && indexesOfTowers.contains(targetTowerNumber)) {
+			if (originTowerNumber != targetTowerNumber) {
+				Integer[] moveFromTo = { originTowerNumber, targetTowerNumber };
+				listOfMoves.add(moveFromTo);
+				Hanoi.moveRing(allTowers[originTowerNumber - 1], allTowers[targetTowerNumber - 1]);
+			} else {
+				System.out.println("Tower numbers you entered are the same!.");
 			}
-			else{
+		} else {
+			System.out.println("One or both of tower numbers are invalid! Towers are numbered 1, 2 and 3.\n");
+		}
+	}
+
+	/**
+	 * moves a ring from origin tower to target tower if origin tower is not
+	 * empty and the moved ring's size is smaller than ring's on top of the
+	 * target tower
+	 * 
+	 * @param origin
+	 * @param target
+	 */
+
+	private static void moveRing(Stack origin, Stack target) {
+		if (origin.isEmpty()) {
+			System.out.println("The tower is empty! " + "\n Choose another move.");
+		} else {
+			if (target.isEmpty() || origin.peek() < target.peek()) {
+				int temp = origin.pop();
+				target.push(temp);
+			} else {
 				System.out.println("You cannot put a bigger ring on a smaller one! \nChoose another move.");
 			}
 
 		}
-		
+
 	}
-	
-	public static void setBoard(){
-		//Hanoi hanoi = new Hanoi();
-		indexesOfTowers.add(1);
-		indexesOfTowers.add(2);
-		indexesOfTowers.add(3);
-		int numberOfRings;
-		System.out.println("Welcome to Hanoi Towers game, you have three empty towers numbered 1, 2 and 3.");
-		System.out.println("How many rings do you want to put on the tower number 1?");
-		Scanner sc=new Scanner(System.in);
-		numberOfRings=sc.nextInt();
-		createTowers(createRings(numberOfRings));
-		System.out.println("Tower 1 looks like this:");
-		originTower.displayVertical();
-		Hanoi.commandToMove();
-		System.out.println("Game is finished, tower 3 is full and looks like this:");
-		targetTower.displayVertical();
-		sc.close();
-	}
-	
-	public static void commandToMove(){
-		Scanner sc=new Scanner(System.in);
-		while(targetTower.getSize()<targetTower.getMaximumStackSize()-1)
-		{
-			//System.out.println(targetTower.getSize());
-		System.out.println("Specify which tower (first number) you want to move the top ring from "
-				+ "and where to move it (second number).\n Separate towers' numbers with whitespaces and press Enter. "
-				+ "Example: 1 3\n");
-		
-		int originTowerNumber, targetTowerNumber;
-		originTowerNumber=sc.nextInt();
-		targetTowerNumber=sc.nextInt();
-		Stack[] allTowers={originTower, intermediateTower, targetTower};
-		//System.out.println(allTowers.length);
-		if(indexesOfTowers.contains(originTowerNumber)&&indexesOfTowers.contains(targetTowerNumber)){
-			Hanoi.moveRing(allTowers[originTowerNumber-1], allTowers[targetTowerNumber-1]);
-		}
-		else{
-			System.out.println("One or both of tower numbers are invalid!\nTower are numbered 1, 2 and 3.\n"
-					+ "Choose another combination.");
-		}
-		
-		
-		}
-		sc.close();
-		}
-		
-		
-	
-	
-	
 
 	/**
-	 * @param args
+	 * prints a list of valid moves made by the player
 	 */
-	public static void main(String[] args) {
-		//Hanoi hanoi = new Hanoi();
-		Hanoi.setBoard();
-
+	private static void printListOfMoves() {
+		System.out.println("Here is the list of your moves:");
+		for (int i = 0; i < listOfMoves.size(); i++) {
+			System.out.println("From " + listOfMoves.get(i)[0] + " to " + listOfMoves.get(i)[1]);
+		}
 	}
+
 }
