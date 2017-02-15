0c4185dbe7bf43b415937d851adabb04bc99d195
Bogna Wróbel <bogna.wrobel@capgemini.com>
2017-02-15 14:39:19 +0100

    part of filtering by user, but for a given id

--- FraudAnalyser/src/com/capgemini/fraudanalyser/FilteringByUser.java
+++ FraudAnalyser/src/com/capgemini/fraudanalyser/FilteringByUser.java
@@ -1,5 +1,34 @@
 package com.capgemini.fraudanalyser;
 
-public class FilteringByUser {
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Set;
+
+public class FilteringByUser implements Filter{
+
+
+	public ArrayList<Transaction> filter(ArrayList<Transaction> transactionsToAnalyze, int userID) {
+		ArrayList<Transaction> transactionsByUser=new ArrayList<>();
+		for (int i=0;i<transactionsToAnalyze.size();i++) {
+			Transaction analyzedTransaction=transactionsToAnalyze.get(i);
+			if(analyzedTransaction.getUserID()==userID){
+				transactionsByUser.add(analyzedTransaction);
+			}
+		}
+		
+		return transactionsByUser;
+	}
+
+	@Override
+	public boolean isSuspect(Transaction transaction) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Set<Transaction> returnListOfSuspectTransactions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
